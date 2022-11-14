f30c8f6e3ed8995ee7de695abb4629df27200667
blazej pospiech <pospiechb@epam.com>
2022-11-15 00:12:17 +0100

    Adjust module methods and tidy up LoginTest.java

--- src/test/java/com/blazej/reportportal2/utils/methodsModuleGui/WaitMethods.java
+++ src/test/java/com/blazej/reportportal2/utils/methodsModuleGui/WaitMethods.java
@@ -1,17 +1,30 @@
 package com.blazej.reportportal2.utils.methodsModuleGui;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
-import static com.blazej.reportportal2.utils.methodsModuleGui.Main.driver;
 
 public class WaitMethods {
+    private static final Logger logger = LogManager.getLogger(WaitMethods.class.getName());
 
-    public static void waitForElement(WebElement webElement, Duration duration){
-        WebDriverWait wait = new WebDriverWait(driver, duration);
+    public static void waitForVisible(WebDriver driver, WebElement webElement) {
+
+        logger.info("Wait for element: " + webElement.toString());
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         wait.until(ExpectedConditions.visibilityOf(webElement));
     }
+
+    public static void waitForVisible(WebDriver driver, By locator) {
+        logger.info("Wait for locator" + locator.toString());
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
 }
